116846d698a2c1c2b2d82dc1efd7a0f3e80bf16d
unknown <sang.kim37.ks47@gmail.com>
2019-07-21 21:18:50 +0700

    fix 3

--- App/controllers/song.js
+++ App/controllers/song.js
@@ -26,34 +26,6 @@ exports.Songs_get_all = (req, res, next) => {
     });
 };
 
-exports.Songs_create_Song = (req, res, next) => {
-  const body = req.body;
-  const song = new Song({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    songLogo: req.files[0].url,
-    songFile: req.files[1].url
-  });
-  song
-    .save()
-    .then(result => {
-      console.log(result);
-      res.status(201).json({
-        message: "Created Song successfully",
-        createdSong: {
-          _id: result._id,
-          name: result.name,
-        }
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
-};
-
 exports.Songs_get_Song = (req, res, next) => {
   const id = req.params.SongId;
   Song.findById(id)
@@ -77,40 +49,4 @@ exports.Songs_get_Song = (req, res, next) => {
     });
 };
 
-exports.Songs_update_Song = (req, res, next) => {
-  const id = req.params.SongId;
-  const updateOps = {};
-  for (const ops of req.body) {
-    updateOps[ops.propName] = ops.value;
-  }
-  Song.update({ _id: id }, { $set: updateOps })
-    .exec()
-    .then(result => {
-      res.status(200).json({
-        message: "Song updated",
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
-};
 
-exports.Songs_delete = (req, res, next) => {
-  const id = req.params.SongId;
-  Song.remove({ _id: id })
-    .exec()
-    .then(result => {
-      res.status(200).json({
-        message: "Song deleted",
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
-};
